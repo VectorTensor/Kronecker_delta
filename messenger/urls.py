a45bacd1dce83549f198a5e5abe15645787839b0
VectorTensor <comppro54@gmail.com>
2021-05-28 15:43:47 +0545

    Completed the messenger part now CSS adding left

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import ShowMessenger,Postdata
+app_name="Mess"
 urlpatterns=[
         path('',ShowMessenger,name='Messenger'),
         path('poster',Postdata,name='poster')
--- messenger/views.py
+++ messenger/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .models import Messages 
+from Question.views import login
 # Create your views here.
 username='Anonymous'
 def ShowMessenger(request):
-    
-    return render(request,'messenger/index.html')
+    print('In ShowMessenger')
+    if request.method == 'POST':
+        global username
+        data=request.POST
+        passw=data['pass']
+        username=data['Name']
+        if passw=="naruto":
+            return render(request,'messenger/index.html')
+        else:
+            return render(request,'Question/login.html')
+
+    else:
+        return render(request,'Question/login.html')
+
 
 def Postdata(request):
     if request.method =='POST':
