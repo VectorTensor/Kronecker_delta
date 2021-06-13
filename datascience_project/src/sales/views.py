ac7a66d661c314c95fb4bac88e95af7a1a34c9fd
Xantosh <xantoshsady@gmail.com>
2021-06-13 23:09:34 +0545

    added data frame

--- datascience_project/src/sales/views.py
+++ datascience_project/src/sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Sale
 from .forms import SalessSearchForm
+import pandas as pd
 
 
 # Create your views here.
@@ -13,6 +14,14 @@ def home_view(request):
     date_to = request.POST.get('date_to')
     chart_type = request.POST.get('chart_type')
     print(date_form,date_to,chart_type)
+
+    qs = Sale.objects.filter(created__date=date_form)
+    obj = Sale.objects.get(id=2)
+    
+    df1 = pd.DataFrame(qs.values())
+    print(df1)
+    print("########")
+    
   
   context = {
     
